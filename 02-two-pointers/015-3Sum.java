7a8c33fd8126b585d0cd8f4339e9ba99ca38e449
DuorouLee <pvietha14@gmail.com>
2026-08-18 14:16:12 +0800

    Update 015-3Sum.java

--- 02-two-pointers/015-3Sum.java
+++ 02-two-pointers/015-3Sum.java
@@ -22,13 +22,11 @@ class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-
-            // 排序后，如果最小的固定数已经大于 0，后面不可能再得到 0
+            // 如果最小的固定数 > 0 后面的三数相加都不可能得到0
             if (nums[i] > 0) {
                 break;
             }
 
-            // 跳过重复的第一个数
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
@@ -37,34 +35,30 @@ class Solution {
             int right = nums.length - 1;
 
             while (left < right) {
+
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
                     results.add(
-                        Arrays.asList(nums[i], nums[left], nums[right])
-                    );
+                            Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
 
-                    // 跳过 left 的重复值
-                    while (left < right && nums[left] == nums[left + 1]) {
+                    while (left < right && nums[left] == nums[left - 1]) {
                         left++;
                     }
 
-                    // 跳过 right 的重复值
-                    while (left < right && nums[right] == nums[right - 1]) {
+                    while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
 
-                    left++;
+                } else if (sum > 0) {
                     right--;
-
-                } else if (sum < 0) {
-                    left++;
                 } else {
-                    right--;
+                    left++;
                 }
             }
         }
-
         return results;
     }
 }
